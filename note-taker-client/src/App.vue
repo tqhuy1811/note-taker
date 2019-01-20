4b4bd3933b183d9bbe691fd7572b6f9ea942e63b
Justin <justin@Justins-MacBook-Air.local>
2019-01-20 21:38:57 +0700

    added card component

--- note-taker-client/src/App.vue
+++ note-taker-client/src/App.vue
@@ -1,5 +1,65 @@
-<template>
-  <div>
-    hello
+<template >
+  <div class="main-app-theme">
+    <div class="container">
+      <div class="columns is-multiline">
+        <div class="column is-12">
+          <h2 class="has-text-centered has-text-dark is-title">Random text to fill out blank space. Because why not ¯\_(ツ)_/¯</h2>  
+        </div>
+        <div class="column is-12">
+          <div class="columns is-centered">
+            <div class="column is-6 column-spacing">
+              <SearchInput />
+            </div>
+          </div>
+        </div>
+        <div class="column is-4">
+          <div class="columns is-multiline column-spacing">
+            <div v-for="n in 10" v-bind:key="n" class="column is-12">
+              <Card />
+            </div>
+          </div>
+        </div>
+        <div class="column is-8 is-hidden-mobile">
+          <div class="columns is-multiline column-spacing">
+            <div v-for="n in 10" v-bind:key="n" class="column is-12">
+              <Card />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
+
+
+<script lang="ts">
+import Vue from 'vue';
+import Card from './components/Card.vue';
+import SearchInput from './components/SearchInput.vue';
+
+export default Vue.extend({
+  components: {
+    Card,
+    SearchInput,
+  },
+});
+</script>
+
+
+
+
+<style>
+  .main-app-theme {
+    background-color: whitesmoke
+  }
+  .container {
+    padding: 20px;
+  }
+  .has-text-centered {
+    font-size: 20px
+  }
+  .column-spacing {
+    padding: 20px
+  }
+</style>
---
+++ note-taker-client/src/components/SearchInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="field">
+    <div class="control has-icons-right">
+      <input 
+        class="input is-rounded" 
+        v-model="searchString" 
+        placeholder="Type me to search stuff"
+      />
+      <span class="icon is-small is-right">
+        <i class="fa fa-search"></i>
+      </span>
+    </div>
+  </div>
+</template>
+
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  name: 'search-input',
+  data() {
+    return {
+      searchString: '',
+    };
+  },
+});
+</script>
+
